83e3f566847b4b4a0fd8f5e3c5c47e80ed491e98
Jörn <>
2022-06-08 09:11:15 +0200

    Little Movement

--- Assets/Scripts/Controller/Player.cs
+++ Assets/Scripts/Controller/Player.cs
@@ -8,14 +8,14 @@ public class Player : MonoBehaviour
     public Color player_color;
     public string id;
 
-    [SerializedField]
+    [SerializeField]
     float movement_speed = 1f;
 
-    RigidBody2 rigid;
+    Rigidbody rigid;
 
     void Start()
     {  
-        rigid = GetComponent<RigidBody2>();
+        rigid = GetComponent<Rigidbody>();
     }
 
     void Update()
@@ -27,13 +27,13 @@ public class Player : MonoBehaviour
     {
         //left right
         if(PlayerMessageManager.buttonHeld(id, "R")){
-            transform.position += Vector2.right * movement_speed;
+            transform.position += Vector3.right * movement_speed;
         } else if(PlayerMessageManager.buttonHeld(id, "L")){
-            transform.position += Vector2.left * movement_speed;
+            transform.position += Vector3.left * movement_speed;
         }
 
-        if(PlayerMessageManager.buttonHeld(id, "U")){
-            
+        if(PlayerMessageManager.buttonHeld(id, "U") || Input.GetKey(KeyCode.Space)){
+            rigid.AddForce(Vector3.up, ForceMode.Impulse);
         }
         
         
